c8249e5eeee4c19fb40850244c3300ebb5c3f2f9
ksgomezp <ksgomezp@eafit.edu.co>
2020-05-12 03:15:31 -0500

    Fixing buyer function

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -3,22 +3,22 @@
 @section("title", __('users.user'))
 
 @section('content')
-<div class="container">
-    <a class="btn btn-primary" href="{{ route('admin.user.buyer') }}">{{ __('users.bestBuyer') }}</a>
-</div>
 
 <div class="container">
     <div class="row justify-content-center">
+
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header font-weight-bold">{{ __('users.users') }}</div>
                 <div class="card-body">
+                <a class="btn btn-primary" href="{{ route('admin.user.buyer') }}">{{ __('users.bestBuyer') }}</a>
+                <td></td>
                     <ul class="list-group">
-                    @foreach($data['users'] as $user)
-                    <a href="{{ route('admin.user.show', $user->getId()) }}">
-                        <li class="list-group-item">{{ $user->getName() }}</li>
-                    </a>
-                    @endforeach
+                        @foreach($data['users'] as $user)
+                        <a href="{{ route('admin.user.show', $user->getId()) }}">
+                            <li class="list-group-item">{{ $user->getName() }}</li>
+                        </a>
+                        @endforeach
                     </ul>
                 </div>
             </div>
